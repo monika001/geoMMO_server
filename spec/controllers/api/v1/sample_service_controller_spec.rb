cc08b69b1efe9d55368edae95e8e39787e083e48
Mateusz Dobek <mat.dobek@gmail.com>
2014-08-06 21:47:20 +0200

    Add simple text for sample_service

--- spec/controllers/api/v1/sample_service_controller_spec.rb
+++ spec/controllers/api/v1/sample_service_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::SampleServiceController, :type => :controller do
-
+  describe 'GET sample' do
+    it 'should respond with status 200' do
+      get :sample
+      expect( response.status ).to eq 200
+    end
+  end
 end
