4716c26b07f112c6fd8a7d81f6a8ad3fb9054181
Mateusz Dobek <mat.dobek@gmail.com>
2014-08-23 20:04:17 +0200

    Set json as default format for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
 
-  namespace :api do
+  namespace :api, defaults: { format: 'json'  } do
     namespace :v1 do
       get 'sample', to: 'sample_service#sample'
     end
--- spec/controllers/api/v1/sample_service_controller_spec.rb
+++ spec/controllers/api/v1/sample_service_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::SampleServiceController, :type => :controller do
   describe 'GET sample' do
     it 'should respond with status 200' do
-      get :sample
+      get :sample, format: :json
       expect( response.status ).to eq 200
     end
   end
