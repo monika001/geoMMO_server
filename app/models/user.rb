7f20bb4ea3c07919ed181f5d1f2e1d6fb9cdd024
Mateusz Dobek <mat.dobek@gmail.com>
2014-08-24 00:01:54 +0200

    User generate token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  before_create :generate_accesss_token
+  
+  validates :token, :email, uniqueness: true
+
+  private
+
+  def generate_accesss_token
+    begin
+      self.token = SecureRandom.hex
+    end while self.class.exists?( token: token  )
+  end
 end
