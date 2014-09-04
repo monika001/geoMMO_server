fe156eb5828cd3c1ccff0e3624e3e80f39cc58c4
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-05 00:10:53 +0200

    Session routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get 'sample', to: 'sample_service#sample'
 
       resource :user, only: [:create]
+      resource :session, only: [:create, :destroy]
     end
   end
 end
