a089b88b488730356d4409fc891f625c4121c167
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-04 22:54:33 +0200

    Update user_controller_spec

--- spec/controllers/api/v1/user_controller_spec.rb
+++ spec/controllers/api/v1/user_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController do
-  describe 'POST create' do
-    it 'respond with status 200- ok' do
+  describe '#create' do
+    before do
       post :create, format: :json
-      expect( response.status ).to eq 200
     end
+
+    it { is_expected.to respond_with(:ok) }
   end
 end
