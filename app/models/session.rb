b35b70bf65e88666582424fdc516b7d53e1537ee
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-06 11:36:16 +0200

    Add Session model

---
+++ app/models/session.rb
@@ -0,0 +1,49 @@
+require "singleton"
+
+class Session
+  include Singleton
+
+  def initialize
+    @store = {}
+  end
+
+  def authenticate_user_with_token(token)
+    store[token]
+  end
+
+  def authenticate_user_with_credentials(credentials)
+    return unless credentials && credentials[:email]
+
+    user = User.authenticate(credentials[:email])
+    regenerate_token user
+
+    user
+  end
+
+  def regenerate_token(user)
+    return unless user
+
+    delete_user_from_session user
+    add_user_to_session user
+  end
+
+  private
+
+  def store
+    @store
+  end
+
+  def token_of(user)
+    store.each { |k,v| return k if v == user }
+  end
+
+  def delete_user_from_session(user)
+    store.delete( token_of user )
+  end
+
+  def add_user_to_session(user)
+    token = SecureRandom.base64
+    store[token] = user
+    token
+  end
+end
