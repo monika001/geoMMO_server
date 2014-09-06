313237da90dcf098afe2e3b423af4124b5c86318
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-06 10:51:03 +0200

    Delete token from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,3 @@
 class User < ActiveRecord::Base
-  before_create :generate_accesss_token
-
-  validates :token, :email, uniqueness: true
-
-  private
-
-  def generate_accesss_token
-    begin
-      self.token = SecureRandom.hex
-    end while self.class.exists?( token: token  )
-  end
+  validates :email, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,9 @@
 require 'rails_helper'
 
 describe User do
-  context 'with the duplicated email' do
-    let!(:user) { create(:user, email: 'sample@sample.com')  }
-    let!(:duplicated_user) { build(:user, email: 'sample@sample.com')  }
+  let(:user) { build(:user) }
 
-    it 'is not valid' do
-      expect(duplicated_user.valid?).to eq false
-    end
-  end
-
-  context 'with the already registered token' do
-    let!(:user) { create(:user, token: '1')  }
-    let!(:duplicated_user) { build(:user, token: '1')  }
-
-    it 'is not valid' do
-      expect(duplicated_user.valid?).to eq false
-    end
+  describe 'validations' do
+    it { is_expected.to validate_uniqueness_of :email }
   end
 end
