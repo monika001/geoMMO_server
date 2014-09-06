04882723bc85aa7b631734664aa25c64d139470f
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-06 15:30:37 +0200

    Add LogUserOut spec

---
+++ spec/services/session/log_user_out_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Session::LogUserOut do
+  let(:user) { create(:user) }
+
+  before do
+    add_user_to_session(user)
+  end
+
+  describe ".call" do
+    it "removes user form session" do
+      old_length = session_length
+      Session::LogUserOut.call user
+
+      expect( session_length ).to eq old_length - 1
+    end
+  end
+end
