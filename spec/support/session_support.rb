589c2b1341dba48fe8cb4d63bfab73f8930934e7
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-07 12:22:10 +0200

    Add spec support for token authentication

--- spec/support/session_support.rb
+++ spec/support/session_support.rb
@@ -11,5 +11,9 @@ module Support
     def add_user_to_session(user)
       Session.instance.send(:add_user_to_session, user)
     end
+
+    def add_token_to_header_of(user)
+      request.headers['HTTP_USER_API_TOKEN'] = token_of(user)
+    end
   end
 end
