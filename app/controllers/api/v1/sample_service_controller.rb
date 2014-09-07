67414d8262fa78203ff5bb28f9cdbb75c26ecb79
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-07 12:23:21 +0200

    Drop authorization for sample controller and Registration

--- app/controllers/api/v1/sample_service_controller.rb
+++ app/controllers/api/v1/sample_service_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::SampleServiceController < ApplicationController
   respond_to :json
 
+  skip_before_action :authenticate_with_token!, only: [:sample]
+
   def sample
     obj = { success: true, info: 'Logged in', data: { sample: 'sample'  }  }
     respond_with obj, status: 200
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
 
+  skip_before_action :authenticate_with_token!, only: [:create]
+
   def create
     obj = {}
     render json: obj, status: :ok
