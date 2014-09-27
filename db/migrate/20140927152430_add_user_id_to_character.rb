ea731f869278f23023398687a7ce7f0734b046f6
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-27 19:49:21 +0200

    Character model now have index and reference to User

---
+++ db/migrate/20140927152430_add_user_id_to_character.rb
@@ -0,0 +1,6 @@
+class AddUserIdToCharacter < ActiveRecord::Migration
+  def change
+    add_column :characters, :user_id, :integer
+    add_index :characters, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927131555) do
+ActiveRecord::Schema.define(version: 20140927152430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,10 @@ ActiveRecord::Schema.define(version: 20140927131555) do
   create_table "characters", force: true do |t|
     t.string  "name"
     t.integer "experience"
+    t.integer "user_id"
   end
+
+  add_index "characters", ["user_id"], name: "index_characters_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
