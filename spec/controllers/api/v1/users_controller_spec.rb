bc6152959627de76566cabeac5d362a9d5c54348
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-27 16:02:54 +0200

    Move shared_examples for user_controler inside user_controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController do
+  shared_examples_for 'respond with current user' do
+    it 'returns current_user id' do
+      expect(json_response[:user][:id]).to eq user.id
+    end
+
+    it 'returns current_user email' do
+      expect(json_response[:user][:email]).to eq user.email
+    end
+
+    it 'returns current_user first_name' do
+      expect(json_response[:user][:first_name]).to eq user.first_name
+    end
+
+    it 'returns current_user last_name' do
+      expect(json_response[:user][:last_name]).to eq user.last_name
+    end
+  end
+
   describe '#show' do
     context 'unauthorized user' do
       it_behaves_like 'unauthorized user', :get, :show
