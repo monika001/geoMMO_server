505a4dfc191b1337765c7974670ddc1d977e62a3
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-27 15:33:30 +0200

    Add character model with spec

---
+++ app/models/character.rb
@@ -0,0 +1,3 @@
+class Character < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+end
---
+++ spec/models/character_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe Character do
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_uniqueness_of :name }
+end
