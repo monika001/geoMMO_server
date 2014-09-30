cd0b4f46d83032d05623543929070456b9b8ddda
Mateusz Dobek <mat.dobek@gmail.com>
2014-09-30 23:03:56 +0200

    Debug server

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,79 +1,73 @@
+require 'singleton'
+
 class Session
-  class << self
-    def authenticate_user_with_token(token)
-      # TODO
-      # Its just for testing purpose
-      # ############################
-      # ############################
-      #if token == 'sample'
-        #user = User.find_by(email: 'sample@sample.co')
-        #if user.nil?
-          #user = User.create(email: 'sample@sample.co', password: 'haslo123', password_confirmation: 'haslo123')
-        #end
-        #store['sample'] = user
-      #end
-      # ############################
-      # ############################
+  include Singleton
 
-      Rails.logger.debug '------------------------------------'
-      Rails.logger.debug '------------------------------------'
-      Rails.logger.debug token
-      Rails.logger.debug store
-      Rails.logger.debug '------------------------------------'
-      Rails.logger.debug '------------------------------------'
-      store[token]
+  def authenticate_user_with_token(token)
+    # TODO
+    # Its just for testing purpose
+    # ############################
+    # ############################
+    if token == 'sample'
+      user = User.find_by(email: 'sample@sample.co')
+      if user.nil?
+        user = User.create(email: 'sample@sample.co', password: 'haslo123', password_confirmation: 'haslo123')
+      end
+      store['sample'] = user
     end
+    # ############################
+    # ############################
 
-    def authenticate_user_with_credentials(credentials)
-      return unless credentials && credentials[:email] && credentials[:password]
+    store[token]
+  end
 
-      email = credentials[:email]
-      password = credentials[:password]
+  def authenticate_user_with_credentials(credentials)
+    return unless credentials && credentials[:email] && credentials[:password]
 
-      user = User.find_by(email: email)
-      user && user.authenticate(password) and
-        token = regenerate_token(user) and
-          [user, token]
-    end
+    email = credentials[:email]
+    password = credentials[:password]
 
-    def regenerate_token(user)
-      return unless user
+    user = User.find_by(email: email)
+    user && user.authenticate(password) and
+      token = regenerate_token(user) and
+        [user, token]
+  end
 
-      delete_user_from_session(user)
-      add_user_to_session(user)
-    end
+  def regenerate_token(user)
+    return unless user
 
-    def destroy_token(user)
-      delete_user_from_session(user)
-    end
+    delete_user_from_session(user)
+    add_user_to_session(user)
+  end
+
+  def destroy_token(user)
+    delete_user_from_session(user)
   end
 
   private
 
-  class << self
-    def store
-      @@store ||= {}
-    end
+  def store
+    @store ||= {}
+  end
 
-    def token_of(user)
-      tokens_of(user).first
-    end
+  def token_of(user)
+    tokens_of(user).first
+  end
 
-    def tokens_of(user)
-      store.select { |_, v| v == user }.keys
-    end
+  def tokens_of(user)
+    store.select { |_, v| v == user }.keys
+  end
 
-    def delete_user_from_session(user)
-      tokens_of(user).each { |k| store.delete(k) }
-    end
+  def delete_user_from_session(user)
+    tokens_of(user).each { |k| store.delete(k) }
+  end
 
-    def add_user_to_session(user)
-      begin
-        token = SecureRandom.base64
-      end while store[token] != nil
+  def add_user_to_session(user)
+    begin
+      token = SecureRandom.base64
+    end while store[token] != nil
 
-      store[token] = user
-      token
-    end
+    store[token] = user
+    token
   end
 end
