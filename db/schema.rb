5db303d7a151170040a633c4803fb62d83916c78
Mateusz Dobek <mat.dobek@gmail.com>
2014-10-01 23:16:23 +0200

    Add token to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927232958) do
+ActiveRecord::Schema.define(version: 20141001195207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20140927232958) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "password_digest"
+    t.string   "token"
   end
 
 end
