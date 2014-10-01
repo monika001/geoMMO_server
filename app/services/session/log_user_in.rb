2d8211918fdc379be7e01f0d02d62685d6258e2c
Mateusz Dobek <mat.dobek@gmail.com>
2014-10-01 23:18:19 +0200

    Update log_user_in service

--- app/services/session/log_user_in.rb
+++ app/services/session/log_user_in.rb
@@ -1,11 +1,34 @@
 class Session::LogUserIn
   class << self
     def with_credentials(credentials)
-      Session.authenticate_user_with_credentials(credentials)
+      return unless credentials && credentials[:email] && credentials[:password]
+
+      email = credentials[:email]
+      password = credentials[:password]
+
+      user = User.find_by(email: email)
+      user && user.authenticate(password) and
+        token = user.regenerate_token and
+          [user, token]
     end
 
     def with_token(token)
-      Session.authenticate_user_with_token(token)
+      return unless token
+
+      # --------------------------
+      # --------------------------
+      if token == 'sample'
+        user = User.find_by(email: 'sample@sample.co')
+        if user.nil?
+          user = User.create(email: 'sample@sample.co', password: 'haslo123', password_confirmation: 'haslo123')
+        end
+        return user
+      end
+      # --------------------------
+      # --------------------------
+      # TODO: Just for tests
+
+      User.find_by(token: token)
     end
   end
 end
