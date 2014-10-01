5cf7dff6dc730ad46e2da078384242fb355019df
Mateusz Dobek <mat.dobek@gmail.com>
2014-10-01 23:17:35 +0200

    Update user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    email Faker::Internet.email
+    email { Faker::Internet.email }
     password 'superSafePassword123'
     password_confirmation 'superSafePassword123'
 
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
   end
 end
