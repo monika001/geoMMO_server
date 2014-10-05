9ce96bec810ce25b1928944d34711c12cf1ac4f6
Mateusz Dobek <mat.dobek@gmail.com>
2014-10-05 14:45:16 +0200

    Add trait to User factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,5 +6,11 @@ FactoryGirl.define do
 
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+
+    trait :with_character do
+      after(:create) do |instance|
+        create_list :character, 1, user: instance
+      end
+    end
   end
 end
